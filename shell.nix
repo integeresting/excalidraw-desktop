b9693ae67cd567865b349e46a1597f01dc71703d
45Hnri <mailhenrimal@web.de>
2026-03-24 16:53:30 +0100

    add dev shell

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+	libs = with pkgs; [
+		alsa-lib
+		cups
+		expat
+		libdrm
+		libgbm
+		libxkbcommon
+		xorg.libX11
+		xorg.libXcomposite
+		xorg.libxcb
+		atk
+		cairo
+		dbus
+		glib
+		gtk3
+		nspr
+		nss
+		pango
+		xorg.libXdamage
+		xorg.libXext
+		xorg.libXfixes
+		xorg.libXrandr
+		libGL
+	];
+in
+	pkgs.mkShell {
+		buildInputs = [ pkgs.nodejs_20 ] ++ libs;
+		shellHook = ''
+		export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH
+		export LD_LIBRARY_PATH=${pkgs.nss}/lib:$LD_LIBRARY_PATH
+		'';
+	}
