6b58268c50845b172a3556147a82f0aa177f0de6
45Hnri <mailhenrimal@web.de>
2026-04-29 13:49:14 +0200

    fix: nix wrap with wayland flags

--- excalidraw-desktop.nix
+++ excalidraw-desktop.nix
@@ -15,17 +15,28 @@ in
 		inherit pname version src;
 		pkgs = pkgs;
 		extraInstallCommands = ''
-				install -m 444 -D ${appimageContents}/${binname}.desktop -t $out/share/applications
-				mv $out/share/applications/${binname}.desktop $out/share/applications/${pname}.desktop
-				substituteInPlace $out/share/applications/${pname}.desktop \
-					--replace 'Exec=AppRun' 'Exec=${pname}'
+      mv $out/bin/excalidraw $out/bin/.excalidraw
+      cat <<EOF > $out/bin/excalidraw
+      #!${pkgs.runtimeShell}
+      FILEPATH=\$1
+      if [ -z \$FILEPATH ]; then
+        FILEPATH=""
+      fi
+      $out/bin/.excalidraw "\$FILEPATH" --ozone-platform=wayland --enable-features=WaylandWindowDecorations
+      EOF
+      chmod +x $out/bin/excalidraw
 
-				# put the icon in 0x0 for some reason, which does not display correctly
-				mkdir -p $out/share/icons/hicolor/512x512
-				cp -r ${appimageContents}/usr/share/icons/hicolor/0x0/* \
-					$out/share/icons/hicolor/512x512
+      install -m 444 -D ${appimageContents}/${binname}.desktop -t $out/share/applications
+      mv $out/share/applications/${binname}.desktop $out/share/applications/${pname}.desktop
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace-fail 'Exec=AppRun' 'Exec=${pname}'
 
-				# normal procedure
-				cp -r ${appimageContents}/usr/share/icons $out/share
-		'';
+      # put the icon in 0x0 for some reason, which does not display correctly
+      mkdir -p $out/share/icons/hicolor/512x512
+      cp -r ${appimageContents}/usr/share/icons/hicolor/0x0/* \
+        $out/share/icons/hicolor/512x512
+
+      # normal procedure
+      cp -r ${appimageContents}/usr/share/icons $out/share
+    '';
 	}
